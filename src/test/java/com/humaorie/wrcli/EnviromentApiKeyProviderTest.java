ae79f724018b5fe1cc281e5ad27f49a5eb108d17
Davide Angelocola <davide.angelocola@gmail.com>
2011-07-31 23:48:42 +0200

    ref: test

--- src/test/java/com/humaorie/wrcli/EnviromentApiKeyProviderTest.java
+++ src/test/java/com/humaorie/wrcli/EnviromentApiKeyProviderTest.java
@@ -1,11 +1,19 @@
 package com.humaorie.wrcli;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class EnviromentApiKeyProviderTest {
 
     @Test(expected = IllegalStateException.class)
-    public void withoutKeyThrows() {
+    public void withoutEnvironmentVariableThrows() {
+        EnviromentApiKeyProvider provider = new EnviromentApiKeyProvider();
+        provider.provideKey();
+    }
+    
+    @Test
+    @Ignore("how to set an environment variable here?")
+    public void withEnvironmentVariableReturnsIt() {
         EnviromentApiKeyProvider provider = new EnviromentApiKeyProvider();
         provider.provideKey();
     }
