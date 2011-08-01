f85aeef26eec087a6e187aeebfead58b42fa7840
Davide Angelocola <davide.angelocola@gmail.com>
2011-08-01 09:46:03 +0200

    ref: removed useless imports

--- src/main/java/com/humaorie/wr/api/JSONParser.java
+++ src/main/java/com/humaorie/wr/api/JSONParser.java
@@ -5,8 +5,6 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
