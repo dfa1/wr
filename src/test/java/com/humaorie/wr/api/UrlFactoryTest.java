48edc6987cd75f0ece69c03723778f201e23701f
Davide Angelocola <davide.angelocola@gmail.com>
2011-08-14 11:30:56 +0200

    enh: coverage

--- src/test/java/com/humaorie/wr/api/UrlFactoryTest.java
+++ src/test/java/com/humaorie/wr/api/UrlFactoryTest.java
@@ -1,8 +1,9 @@
 package com.humaorie.wr.api;
 
 import java.net.MalformedURLException;
-import org.junit.Ignore;
+import java.net.URL;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class UrlFactoryTest {
 
@@ -37,6 +38,15 @@ public class UrlFactoryTest {
         final StubApiKeyProvider apiKeyProvider = new StubApiKeyProvider();
         final UrlFactory urlFactory = new UrlFactory(apiKeyProvider);
         urlFactory.createUrl("enit", "");
+    }
+
+    @Test
+    public void acceptNonEmptyDictAndWord() throws MalformedURLException {
+        final StubApiKeyProvider apiKeyProvider = new StubApiKeyProvider();
+        final UrlFactory urlFactory = new UrlFactory(apiKeyProvider);
+        final URL createdUrl = urlFactory.createUrl("enit", "dog");
+        final URL expectedUrl = new URL("http://api.wordreference.com/0.8/key/json/enit/dog");
+        Assert.assertEquals(expectedUrl, createdUrl);
     }
 
     public static class StubApiKeyProvider implements ApiKeyProvider {
