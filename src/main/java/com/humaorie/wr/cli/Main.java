08c1fc62e1014f8ad4846be6067b06a239c9afd8
Davide Angelocola <davide.angelocola@gmail.com>
2012-05-02 20:03:46 +0200

    ref: cosmetics

--- src/main/java/com/humaorie/wr/cli/Main.java
+++ src/main/java/com/humaorie/wr/cli/Main.java
@@ -1,22 +1,17 @@
 package com.humaorie.wr.cli;
 
-import com.humaorie.wr.api.JsonParserCursedByLeChuck;
-import com.humaorie.wr.api.Parser;
-import com.humaorie.wr.api.DefaultWordReference;
-import com.humaorie.wr.api.FileApiKeyProvider;
-import com.humaorie.wr.api.GzippedJsonOverHttpRepository;
-import com.humaorie.wr.api.UrlFactory;
+import com.humaorie.wr.api.*;
 import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
         final File apiKeyFile = new File(System.getProperty("user.home"), ".wrcli");
-        final FileApiKeyProvider apiKeyProvider = new FileApiKeyProvider(apiKeyFile);
+        final ApiKeyProvider apiKeyProvider = new FileApiKeyProvider(apiKeyFile);
         final UrlFactory urlFactory = new UrlFactory(apiKeyProvider);
-        final GzippedJsonOverHttpRepository repository = new GzippedJsonOverHttpRepository(urlFactory);
+        final Repository repository = new GzippedJsonOverHttpRepository(urlFactory);
         final Parser parser = new JsonParserCursedByLeChuck();
-        final DefaultWordReference wordReference = new DefaultWordReference(repository, parser);
+        final WordReference wordReference = new DefaultWordReference(repository, parser);
         final CommandLineClient cli = new CommandLineClient(wordReference);
         final int status = cli.run(args);
         System.exit(status);
