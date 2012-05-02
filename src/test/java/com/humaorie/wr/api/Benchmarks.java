15e97d24eb9c540e558976be0a3866a374757442
Davide Angelocola <davide.angelocola@gmail.com>
2012-05-02 20:38:23 +0200

    enh: improved benchmarks

--- src/test/java/com/humaorie/wr/api/Benchmarks.java
+++ src/test/java/com/humaorie/wr/api/Benchmarks.java
@@ -4,31 +4,36 @@ import org.junit.Test;
 
 public class Benchmarks {
 
-    private final int REPEAT = 30;
+    private final String TESTING_API_KEY = "2f6ce"; // this is a real key, please don't abuse
 
     @Test
     public void lookupRepeatedlyWithoutGzip() {
-        final ConstantApiKeyProvider costantApiKeyProvider = new ConstantApiKeyProvider("2");
-        final UrlFactory urlFactory = new UrlFactory(costantApiKeyProvider);
+        final ApiKeyProvider apiKeyProvider = new ConstantApiKeyProvider(TESTING_API_KEY);
+        final UrlFactory urlFactory = new UrlFactory(apiKeyProvider);
         final Repository repository = new JsonOverHttpRepository(urlFactory);
         final Parser parser = new JsonParserCursedByLeChuck();
-        final DefaultWordReference wordReference = new DefaultWordReference(repository, parser);
-
-        for (int i = 0; i < REPEAT; i++) {
-            wordReference.lookup("enit", "run");
-        }
+        final WordReference wordReference = new DefaultWordReference(repository, parser);
+        final long elapsed = benchmarkRepeatedLookup(wordReference);
+        System.out.printf("without GZIP: %sms%n", elapsed);
     }
 
     @Test
     public void lookupRepeatedlyWithGzip() {
-        final ConstantApiKeyProvider costantApiKeyProvider = new ConstantApiKeyProvider("2");
-        final UrlFactory urlFactory = new UrlFactory(costantApiKeyProvider);
+        final ApiKeyProvider apiKeyProvider = new ConstantApiKeyProvider(TESTING_API_KEY);
+        final UrlFactory urlFactory = new UrlFactory(apiKeyProvider);
         final Repository repository = new GzippedJsonOverHttpRepository(urlFactory);
         final Parser parser = new JsonParserCursedByLeChuck();
-        final DefaultWordReference wordReference = new DefaultWordReference(repository, parser);
+        final WordReference wordReference = new DefaultWordReference(repository, parser);
+        final long elapsed = benchmarkRepeatedLookup(wordReference);
+        System.out.printf("with GZIP   : %sms%n", elapsed);
+    }
 
-        for (int i = 0; i < REPEAT; i++) {
-            wordReference.lookup("enit", "run");
+    private long benchmarkRepeatedLookup(WordReference sut) {
+        final int repeat = 10;
+        final long start = System.currentTimeMillis();
+        for (int i = 0; i < repeat; i++) {
+            sut.lookup("enit", "word");
         }
+        return System.currentTimeMillis() - start;
     }
 }
